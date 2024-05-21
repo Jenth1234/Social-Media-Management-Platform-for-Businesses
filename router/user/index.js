6b8324ca1db51227183ee00fcc5747946fb568b5
thong <thongviet@gmail.com>
2024-05-21 13:54:50 +0700

    update router

--- router/user/index.js
+++ router/user/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const user_controller = require('../../controllers/user/user.controller');
+const verifyToken = require("../../middleware/verifyToken");
 
-router.put('/editUser/:id',user_controller.editUser);//CHECK LAI LỖI 
-router.delete('/deleteUser/:id',user_controller.deleteUser);//check lại lỗi
-router.get('/getUsers',user_controller.getUsers);
+router.put('/editUser/:id', verifyToken,user_controller.editUser);//CHECK LAI LỖI 
+router.delete('/deleteUser/:id',verifyToken,user_controller.deleteUser);//check lại lỗi
+router.get('/getUsers',verifyToken,user_controller.getUsers);
 router.post("/register", user_controller.registerUser);
 router.post("/loginUser", user_controller.login);
 
