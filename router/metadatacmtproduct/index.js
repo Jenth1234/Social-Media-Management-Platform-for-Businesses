02100b11a19b0cd5c0587854e608b826c224c801
thaontp <thaob2014788@student.ctu.edu.vn>
2024-06-03 15:25:56 +0700

    controllers/metadatacmtproduct/metadatacmtproduct.controller.js

--- router/metadatacmtproduct/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const metadatacmtproductController = require('../../controllers/metadatacmtproduct/metadatacmtproduct.controller');
-const verifyToken = require('../../middleware/verifyToken');
-
-router.post('/updateCommentCount', metadatacmtproductController.updateCommentCountController);
-
-module.exports = router;
